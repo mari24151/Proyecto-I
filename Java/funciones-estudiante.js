742e816b73e813c434aca092b117ea4e384c7a62
mari24151 <ale.24151@gmail.com>
2014-07-26 15:13:44 -0500

    creacion de funciones
    se  trato de crear las funciones necesarias

---
+++ Java/funciones-estudiante.js
@@ -0,0 +1,39 @@
+//guardar estudiante
+function guardarEstudiante() {
+    // obtener datos del form
+    var name = document.getElementById('name').value,
+        apellidos = document.getElementById('apellidos').value,
+        ced = document.getElementById('ced').value,
+        carr = document.getElementById('carr').value,
+        sex = document.getElementById('nivel').value;
+        //foto = document.getElementById('first_name').value;
+
+    // crear objeto estudiante
+    var estudiante = { "name": name, "apellidos": apellidos, "ced": ced, "carr": carr, "nivel": sex };
+    
+    // leer los estudiantes de localstorage
+    var estudiante = JSON.parse(localStorage.getItem('estudiante'));
+    if (estudiante === null) {
+        estudiante = [];
+    }
+
+    // agregar el estudiante
+    estudiante.push(estudiante);
+
+    // volver guardar en localstoraage
+    localStorage.setItem('estudiante',JSON.stringify(estudiante));
+}
+
+
+//Eliminar estudiantes 
+function eliminarEstudiante(element) {
+    if (confirm('Deseas eliminar estudiante')) {
+        alert('El estudiante: '+element.value+ ' ha sido eliminado');
+        document.location.href='Editar-estudiante.html';
+    } else {
+        document.location.href='Estudiantes.html';
+    }
+}
+
+
+// Editar estudiante
