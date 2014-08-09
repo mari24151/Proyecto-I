66942c02aa25279fe94e501840d480255c604ae8
mari24151 <ale.24151@gmail.com>
2014-08-09 15:21:25 -0500

    Cambios

--- Java/funciones-carreras.js
+++ Java/funciones-carreras.js
@@ -38,7 +38,7 @@ function editarCarreras(){
 
 
 // Mostrar Carreras
-function mostrarCarrera(bbcodigo, bbnombre){
+function mostrarCarrera(){
 
     
         var columnas = "<tr><th>Codigo</th><th>Nombre</th><th>Opciones</th></tr>";
@@ -66,15 +66,100 @@ function mostrarCarrera(bbcodigo, bbnombre){
         document.getElementById("tabla-carreras").innerHTML = carrera;  
 }
 
+//editar carreras
+function editarCarrera()
+
+{
+
+    var codigo_carrera;
+    var nombre_carrera;
+    var carreras = JSON.parse(localStorage.getItem('carreras'));
+
+    var codigo_carrera = window.location.href.slice(window.location.href.indexOf('=') + 1);
+
+
+
+         for (var i = 0; i < carreras.length; i++) 
+           {
+
+                if(carreras[i] != undefined)
+               {
+
+
+                 if(carreras[i].codigo == codigo_carrera)
+                   {
+
+                     nombre_carrera = carreras[i].nombre;
+
+                    }
+                }
+           };
+
+
+    document.getElementById("codigo").value = codigo_carrera;
+    document.getElementById("nombre").value = nombre_carrera;
+
+
+        $("#btn-editar").click(function() 
+
+            {
+
+                for (var i = 0; i < carreras.length; i++) 
+                {
+
+                    if(carreras[i] != undefined)
+                       {
+
+
+                          if(carreras[i].codigo == codigo_carrera)
+                             {
+
+                                carreras[i].nombre = document.getElementById("nombre").value;
+                             }
+                        }
+                };
+
+              localStorage.setItem('carreras',JSON.stringify(carreras));
+
+              document.getElementById("mensaje").innerHTML = '<div class="alert alert-info" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>Carrera editada con exito!</div>';
+            });
+}
+
 
 //Eliminar Carreras 
-function eliminarCarrera(element) {
-    if (confirm('Deseas eliminar la carrara selecionada')) {
-        alert('La carrera: '+element.value+ ' ha sido eliminado');
-        document.location.href='Editar-carreras.html';
-    } else {
-        document.location.href='Carreras.html';
-    }
+function  eliminarCarrera()
+{
+
+
+        $(".eliminar").click(function() 
+            {
+
+                var codigo_carrera =  $(this).attr("id");
+
+                var carreras = JSON.parse(localStorage.getItem('carreras'));
+
+                for (var i = 0; i < carreras.length; i++) 
+                {
+
+                   if(carreras[i] != undefined)
+                    {
+
+                      if(carreras[i].codigo == codigo_carrera)
+                        {
+
+                        delete carreras[i];
+                        }
+                    }
+                };
+
+               localStorage.setItem('carreras',JSON.stringify(carreras));
+
+               document.getElementById("mensaje").innerHTML = '<div class="alert alert-danger" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>Carrera eliminada con exito!</div>';
+        
+                cargarCarrera();
+
+                eliminarCarrera();
+            });       
 }
 
 
