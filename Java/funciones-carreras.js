3b7a2273b717939d5853c473a39f9466d447844b
mari24151 <ale.24151@gmail.com>
2014-08-09 12:34:55 -0500

    Cambios

--- Java/funciones-carreras.js
+++ Java/funciones-carreras.js
@@ -15,7 +15,7 @@ function guardarCarrera() {
     var carrera = { "codigo": codigo, "nombre": nombre };
     
     // leer los estudiantes de localstorage
-    var carreras = JSON.parse(localStorage.getItem('carrera1'));
+    var carreras = JSON.parse(localStorage.getItem('carreras'));
     if (carreras === null) {
         carreras = [];
     }
@@ -24,7 +24,8 @@ function guardarCarrera() {
     carreras.push(carrera);
 
     // volver guardar en localstoraage
-    localStorage.setItem('carrera',JSON.stringify(carreras));
+    localStorage.setItem('carreras',JSON.stringify(carreras));
+
 }
 
 
@@ -37,10 +38,32 @@ function editarCarreras(){
 
 
 // Mostrar Carreras
-function mostrarCarrera(){
-
+function mostrarCarrera(bbcodigo, bbnombre){
 
     
+        var columnas = "<tr><th>Codigo</th><th>Nombre</th><th>Opciones</th></tr>";
+
+        var carreras = JSON.parse(localStorage.getItem('carreras'));
+        var carrera = columnas;
+
+        for (var i = 0 ; i < carreras.length; i++) {
+            
+            if(carreras[i] != undefined)
+            {
+            carrera += "<tr>";
+            carrera += '<td class="lbl-codigo"><a href="#">'+carreras[i].codigo+'</a></td>';
+            carrera +=  '<td class="lbl-nombre">'+carreras[i].nombre+'</td>';
+            carrera += "<td>";
+            carrera += '<div class="btn-group">';
+            carrera += ' <button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown">Opciones<span class="caret"></span></button>';
+            carrera += '<ul class="dropdown-menu" role="menu"><li><a class="editar" id="'+carreras[i].codigo+'" href="editar.html?codigo='+carreras[i].codigo+'">Editar</a></li><li><a class="eliminar" id="'+carreras[i].codigo+'" href="#">Eliminar</a></li></ul>';
+            carrera += '</div>';
+            carrera += "</td>";
+            carrera += "</tr>";
+            }
+        };
+
+        document.getElementById("table-carreras").innerHTML = carrera;  
 }
 
 
