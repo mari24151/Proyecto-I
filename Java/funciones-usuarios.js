4f18cae8aab7b0ab23834756c06b96945dfe7768
mari24151 <ale.24151@gmail.com>
2014-08-09 16:43:17 -0500

    Cambios

--- Java/funciones-usuarios.js
+++ Java/funciones-usuarios.js
@@ -25,7 +25,34 @@ function guardarUsuario() {
 
     // volver guardar en localstoraage
     localStorage.setItem('usuarios',JSON.stringify(usuarios));
-
 }
 
+// Mostrar Usuarios en la tabla 
+function mostrarUsuarios(){
+
+    
+        var columnas = "<tr><th>Cedula</th><th>Nombre</th><th>Opciones</th></tr>";
+
+        var carreras = JSON.parse(localStorage.getItem('carreras'));
+        var carrera = columnas;
+
+        for (var i = 0 ; i < carreras.length; i++) {
+            
+            if(carreras[i] != undefined)
+            {
+            carrera += "<tr>";
+            carrera += '<td class="lbl-codigo"><a href="#">'+carreras[i].codigo+'</a></td>';
+            carrera +=  '<td class="lbl-nombre">'+carreras[i].nombre+'</td>';
+            carrera += "<td>";
+            carrera += '<div class="btn-group">';
+            carrera += ' <button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown">Opciones<span class="caret"></span></button>';
+            carrera += '<ul class="dropdown-menu" role="menu"><li><a class="editar" id="'+carreras[i].codigo+'" href="editar.html?codigo='+carreras[i].codigo+'">Editar</a></li><li><a class="eliminar" id="'+carreras[i].codigo+'" href="#">Eliminar</a></li></ul>';
+            carrera += '</div>';
+            carrera += "</td>";
+            carrera += "</tr>";
+            }
+        };
+
+        document.getElementById("tabla-carreras").innerHTML = carrera;  
+}
 
