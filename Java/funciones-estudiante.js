85389d50a5b429b5935982e5c46f974b85a6529d
mari24151 <ale.24151@gmail.com>
2014-08-18 13:06:14 -0500

    Revert "cambios"
    This reverts commit 9fd4a83bdf865e15b1ba463a710a3709135ab585.

--- Java/funciones-estudiante.js
+++ Java/funciones-estudiante.js
@@ -6,10 +6,6 @@ function prepararEstudiante() {
     $(".editar").click(function() {
         CargarValoresEstudiante(this.id);
     });
-
-    $("#vista").click(function() {
-        VistaEstudiante(this.text);
-    });
 }
 
 //guardar Estudiantes
@@ -65,7 +61,7 @@ function mostrarEstudiante() {
         if (estudiantes[i] != undefined) {
             estudiante += "<tr>";
             estudiante += '<td class="lbl-imagen"><img width="100px" heigth="100px" src="Imagenes/'+estudiantes[i].imagen+'"></img></td>';
-            estudiante += '<td class="lbl-cedula"><a id="vista" data-toggle="modal" data-target="#vista-estudiante">' + estudiantes[i].cedula + '</a></td>';
+            estudiante += '<td class="lbl-cedula"><a data-toggle="modal" data-target="#miventana">' + estudiantes[i].cedula + '</a></td>';
             estudiante += '<td class="lbl-nobre">' + estudiantes[i].nombre + '</td>';
             estudiante += '<td class="lbl-carrera">' + estudiantes[i].carrera + '</td>';
             estudiante += '<td class="lbl-role">' + estudiantes[i].role + '</td>';
@@ -226,45 +222,5 @@ function cargarCarrera() {
     //mostrarlos en la tabla
     document.getElementById("carrera").innerHTML = valores;
 
-
-}
-
-function VistaEstudiante(codigo) {
-
-    var cedula_estudiante;
-    var nombre_estudiante;
-    var carrera_estudiante;
-    var role_estudiante;
-    var imagen_estudiante;
-
-    var estudiantes = JSON.parse(localStorage.getItem('estudiantes'));
-
-    var cedula_estudiante = codigo;
-
-
-
-    for (var i = 0; i < estudiantes.length; i++) {
-
-        if (estudiantes[i] != undefined) {
-            if (estudiantes[i].cedula == cedula_estudiante) {
-
-                nombre_estudiante = estudiantes[i].nombre;
-                carrera_estudiante = estudiantes[i].carrera;
-                role_estudiante = estudiantes[i].role;
-                imagen_estudiante= estudiantes[i].imagen;
-
-            }
-
-        }
-
-
-    };
-
-
-    document.getElementById("cedula-vista").value = cedula_estudiante;
-    document.getElementById("nombre-vista").value = nombre_estudiante;
-    document.getElementById("carrera-vista").value = carrera_estudiante;
-    document.getElementById("role-vista").value = role_estudiante;
-    document.getElementById("imagen-vista").src= "Imagenes/"+imagen_estudiante;
 
 }
