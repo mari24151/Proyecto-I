c75151c8450abd0bf42a352e6ecab83d43f05abb
mari24151 <ale.24151@gmail.com>
2014-08-18 21:50:30 -0500

    cambios

--- Java/funciones-login.js
+++ Java/funciones-login.js
@@ -7,22 +7,25 @@ function prepararLogin() {
 
 // funcion donde se revisa el usuario y contraseña
 function cargar() {
-   
-   // tomar los valores de las cajas
+debugger;
+    // tomar los valores de las cajas
     var usuario = document.getElementById('usuario').value;
-    	contrasena = document.getElementById('contrasena').value;
-    
-    
-    var usuarios = JSON.parse(localStorage.getItem('usuarios'));
+    contrasena = document.getElementById('contrasena').value;
 
+    // leer valores
+    var usuarios = JSON.parse(localStorage.getItem('usuarios'));
+    // recore toda la info 
     for (var i = 0; i < usuarios.length; i++) {
 
         if (usuarios[i] != undefined) {
 
-            if ((usuarios[i].usuario == usuario) && (usuarios[i].contrasena == contrasena)) {
-
+            // busca si es correcto la descripcion
+            if (usuarios[i].alias == usuario && usuarios[i].contrasena == contrasena) {
+                // envia al dashboard en caso correcto
                 location.href = "Dahsboard.html";
+                break;
             } else {
+            	//muestra alerta en caso contrario
                 alert('Su Usuario o Contraseña es incorrecta');
             }
         }
